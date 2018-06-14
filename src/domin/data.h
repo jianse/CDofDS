8d58c46935ace4aea94bcedee9b58910584500e8
李强 <1484190946@qq.com>
2018-06-15 00:29:26 +0800

    定义数据持久化就扣与持久化规则

--- src/domin/data.h
+++ src/domin/data.h
@@ -7,8 +7,24 @@
  * 定义存入数据库的数据类型
  */
 
+/**
+ * 这接口规定了数据实体对象的序列化方式
+ * 实现这个就扣规定的规则，可以让数据以一致的形式写入到磁盘上
+ *
+ */
+#include "dominInt.h"
+#ifdef getData
+#undef getData
+#define getData getDataInt
+#else
+#define getData getDataInt
+#endif
 
 
-struct data {
-	char * data;
-};
+#ifdef T
+#undef T
+#define T int
+#else
+#define T int
+#endif
+char * getData(T adt_data);
