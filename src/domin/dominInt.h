e23552a6a890fec537526b029f3b749d7b9c18a3
李强 <1484190946@qq.com>
2018-06-15 00:28:15 +0800

    统一数据类型

---
+++ src/domin/dominInt.h
@@ -0,0 +1,13 @@
+/*
+ * dominInt.h
+ *
+ *  Created on: 2018年6月14日
+ *      Author: lee
+ */
+
+#ifndef DOMIN_DOMININT_H_
+#define DOMIN_DOMININT_H_
+
+typedef char * String;
+
+#endif /* DOMIN_DOMININT_H_ */
